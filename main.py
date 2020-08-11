2c7963dfe13d251ff5b13e093e06f4b4ef3871d0
jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2020-08-11 22:05:10 +0800

    add route /status-icon

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, send_file
+from mcstatus import MinecraftServer
+
+__author__ = 'Xiao_Jin'
+
+app = Flask(__name__)
+
+
+class icons(object):
+    '''Response icons'''
+    offline: str = './icons/offline.svg'
+    online: str = './icons/online.svg'
+    warning: str = './icons/warning.svg'
+
+
+# https://example.com/status-icon?addr=mc.hypixel.net
+@app.route('/status-icon', methods=['GET'])
+def route_status():
+    _httpGetArgs = request.args
+    if 'addr' in _httpGetArgs:
+        serverAddr: str = _httpGetArgs.get('addr')
+        resIcon = icons.online if isServerOnline(serverAddr) else icons.offline
+        return send_file(resIcon)
+    else:
+        return send_file(icons.warning)
+
+
+@app.after_request
+def apply_caching(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Cache-Control'] = 'no-store'
+    return response
+
+
+def isServerOnline(_serverAddr: str) -> bool:
+    '''Check whether `_serverAddr` is online.
+
+    Args:
+        _serverAddr: Minecraft Server address'''
+    server = MinecraftServer.lookup(_serverAddr)
+    try:
+        server.status()
+        return True
+    except Exception:
+        return False
+
+
+if __name__ == '__main__':
+    app.run(port=85)
